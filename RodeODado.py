7153c83914491632d4a194bdfc04db5e0e98c451
Thiago Costa Pereira <thiago.devpython@gmail.com>
2021-12-30 10:15:11 -0300

    Add informações do autor e melhoramento do Import Tkinter

--- RodeODado.py
+++ RodeODado.py
@@ -1,30 +1,32 @@
+# Autor: Thiago Costa Pereira
+# Email: thiago.devpython@gmail.com
+
+
 #  Passo 1: importar bibliotecas que irei usar
 import random
-import tkinter
+from tkinter import *
 from PIL import Image, ImageTk
 
-# import random
-
 #  Passo 2: criar a janela com título do projeto
-janela = tkinter.Tk()
+janela = Tk()
 janela.geometry('400x400')
 janela.title('Rode o Dado')
 
 #  Passo 3
 # Label introdução da Janela
-label_texto = tkinter.Label(janela, text='')
+label_texto = Label(janela, text='')
 label_texto.pack()
 # Label para formatação da Janela
-label_texto1 = tkinter.Label(janela, text='SIMULADOR DE DADO',
-                             fg='black',
-                             font='Arial 16 bold italic')
+label_texto1 = Label(janela, text='SIMULADOR DE DADO',
+                     fg='black',
+                     font='Arial 16 bold italic')
 label_texto1.pack()
 
-#  Passo 4: tratar imagens dos dados a serem sorteadas.
+#  Passo 4: imagens dos dados a serem sorteadas.
 dados = ['imagens_dado/dado_1.png', 'imagens_dado/dado_2.png', 'imagens_dado/dado_3.png', 'imagens_dado/dado_4.png',
          'imagens_dado/dado_5.png', 'imagens_dado/dado_6.png']
 dado_capa = ImageTk.PhotoImage(file='imagens_dado/Dado.png')
-label_capa = tkinter.Label(janela, image=dado_capa)
+label_capa = Label(janela, image=dado_capa)
 label_capa.image = dado_capa
 label_capa.pack(expand=True)
 
@@ -38,7 +40,7 @@ def rodar_dado():
 
 
 # Adicionando botão de ação
-botao = tkinter.Button(janela, text='Rode o dado', command=rodar_dado)
+botao = Button(janela, text='Rode o dado', command=rodar_dado)
 botao.pack(expand=True)
 
 janela.mainloop()
